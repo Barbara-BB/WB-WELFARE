52acf3b291eb68d4d91e1b615f8ba5eee870ba26
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2021-03-16 00:30:03 -0300

    Configurando projeto
    Baixado projeto maven
    JAVA 15
    Dependendcies: Spring Boot DevTools, Spring Web, Thymeleaf

---
+++ welfare/src/test/java/com/wb/welfare/WelfareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wb.welfare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelfareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
