26dded9889c1f591e6e7205f3fdf6cdf5ead74bf
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2021-04-13 01:06:08 -0300

    SalaoController

--- welfare/src/main/java/com/wb/welfare/controller/SalaoController.java
+++ welfare/src/main/java/com/wb/welfare/controller/SalaoController.java
@@ -4,14 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-	@GetMapping("/home")
+public class SalaoController {
+	@GetMapping("/salao")
 	public String paginaInicial() {
-		return "home";
+		return "salao";
 	}
 
 	}
-
-
-
-
