0f8e0427c4bccbac19367a426bdb1662cf461d92
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2021-04-28 01:30:58 -0300

    Editar_Excluir_Buscar Cliente

--- welfare/src/main/java/com/wb/welfare/repository/ClienteRepository.java
+++ welfare/src/main/java/com/wb/welfare/repository/ClienteRepository.java
@@ -1,5 +1,8 @@
 package com.wb.welfare.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,5 +12,8 @@ import com.wb.welfare.dominio.Cliente;
 @Repository
 @Transactional
 public interface ClienteRepository extends CrudRepository<Cliente,Long>{
+	
+	@Query("select p from Cliente p where p.nome like %?1%")
+	List<Cliente> findClienteByName(String nome);
 
 }
