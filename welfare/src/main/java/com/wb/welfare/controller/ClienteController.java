fa98aef0cf0f9fde492fdecd9b466cba6d6ba3a5
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2021-04-28 17:26:25 -0300

    telas separadas

--- welfare/src/main/java/com/wb/welfare/controller/ClienteController.java
+++ welfare/src/main/java/com/wb/welfare/controller/ClienteController.java
@@ -39,45 +39,7 @@ public class ClienteController {
 
 	}
 	
-	@RequestMapping(method = RequestMethod.GET, value="/listaclientes")
-	public ModelAndView clientes() {
-		
-		ModelAndView andView = new ModelAndView("cadastro/cadastrocliente");
-		Iterable<Cliente> clientesIt = clienteRepository.findAll();
-		andView.addObject("clientes", clientesIt);
-		andView.addObject("clientebj",new Cliente());
-		return andView;
-		
-	}
-	
-	@GetMapping("/editarcliente/{idcliente}")
-	public ModelAndView editar(@PathVariable("idcliente") Long idcliente) {
-		Optional<Cliente> cliente = clienteRepository.findById(idcliente);
-		ModelAndView modelAndView = new ModelAndView("cadastro/cadastrocliente");
-		modelAndView.addObject("clientebj",cliente.get());
-		return modelAndView;
-		
-		
-	}
-	
-	@GetMapping("/excluircliente/{idcliente}")
-	public ModelAndView excluir(@PathVariable("idcliente") Long idcliente) {
-		clienteRepository.deleteById(idcliente);
-		ModelAndView modelAndView = new ModelAndView("cadastro/cadastrocliente");
-		modelAndView.addObject("clientes",clienteRepository.findAll());
-		return modelAndView;
-		
-		
-	}
-	
-	@PostMapping("**/pesquisarcliente")
-	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
-		ModelAndView modelAndView = new ModelAndView("cadastro/cadastrocliente");
-		modelAndView.addObject("clientes", clienteRepository.findClienteByName(nomepesquisa));
-		modelAndView.addObject("clientebj",new Cliente());
-		return modelAndView;
-		
-	}
+
 
 
 }
