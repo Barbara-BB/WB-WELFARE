114908a8ea1dbdcf0927e77314072ec2574933ff
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2021-04-29 18:51:18 -0300

    melhorando css cadastrocliente

--- welfare/src/main/java/com/wb/welfare/controller/ClienteController.java
+++ welfare/src/main/java/com/wb/welfare/controller/ClienteController.java
@@ -1,15 +1,11 @@
 package com.wb.welfare.controller;
 
-import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.wb.welfare.dominio.Cliente;
